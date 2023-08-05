c778e35c6a2251bbf5bde81494a5f0bd61634269
AmirmahdiShayankhwah <amyrmhdyshaya7@gmail.com>
2023-08-05 21:52:09 +0330

    first commit

---
+++ prodoct cala/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prodoct_cala
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            lobtob lap = new lobtob();
+            lap.submitcala();
+            lap.deletcala();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ prodoct cala/abccala.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prodoct_cala
+{
+     public abstract class abccala
+    {
+        int id;
+        string namecala;
+        string medin;
+        int price;
+        string color;
+
+        public void submitcala()
+        {
+            id = int.Parse(Console.ReadLine());
+        }
+        public void deletcala()
+        {
+            namecala = Console.ReadLine();
+        }
+        public void submitcolor()
+        {
+            color = Console.ReadLine();
+        }
+        public void selecalaonline()
+        {
+            
+        }
+        public void submitnamecala()
+        {
+            namecala = Console.ReadLine();
+        }
+        public void submitpricecala()
+        {
+
+        }
+        public void submitgraphiccala()
+        {
+
+        }
+       
+    }
+    public class lobtob : abccala
+    {
+
+    }
+}
